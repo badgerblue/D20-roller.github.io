54113484d3ce94a63240f700f5cb5dd6722e45d8
Robby <robbywilcox82@gmail.com>
2020-02-24 19:18:05 -0600

    Code cleanup
    deleted half my JS code and converted all the buttons to one function that would work for all. This allows me to add up to ten rolls easy

--- script.js
+++ script.js
@@ -1,126 +1,29 @@
-//rolls a D20 and displays it under the dice and in the log. functions below do the same for other numbers
-//-----------------------------------D20----------------------------------------------------------------------------------------
-function d201() {
-    moveLog();
-    let roll = Math.floor(Math.random()*20 + 1);
-    document.getElementById("log1").innerHTML = ' (D20: ' + roll + ') = ' + roll;
+//rolls a dice with X sides and displays it in the results.
+function rollX(sides, times){
+  moveLog();
+  //first roll
+  let roll = Math.floor(Math.random()*sides + 1);
+  //if it is multiple rolls
+  if (times > 1){
+    let total = 0;
+    //cycle through all times to roll and add the roll text to the log
+    for(let i = 0; i < times; i++){
+      let temp = Math.floor(Math.random()*sides + 1);
+      if(i ==0){
+        document.getElementById("log1").innerHTML = ' (D' + sides + ': ' + temp + ') '
+      }
+      else{
+        document.getElementById("log1").innerHTML = document.getElementById("log1").innerHTML + ' (D' + sides + ': ' + temp + ') '
+      }
+      total += temp;
+    }
+    document.getElementById("log1").innerHTML = document.getElementById("log1").innerHTML + ' = ' + total;
   }
-
-  function d202() {
-    moveLog();
-    let roll1 = Math.floor(Math.random()*20 + 1);
-    let roll2 = Math.floor(Math.random()*20 + 1);
-    document.getElementById("log1").innerHTML = '(D20: ' + roll1 + ') (D20: ' + roll2 + ') = ' +  (roll1 + roll2);
+  //one roll
+  else{
+    document.getElementById("log1").innerHTML = ' (D' + sides + ': ' + roll + ') = ' + roll;
   }
-
-  function d203() {
-    moveLog();
-    let roll1 = Math.floor(Math.random()*20 + 1);
-    let roll2 = Math.floor(Math.random()*20 + 1);
-    let roll3 = Math.floor(Math.random()*20 + 1);
-    document.getElementById("log1").innerHTML = '(D20: ' + roll1 + ') (D20: ' + roll2 + ') (D20: ' + roll3 + ') = ' +  (roll1 + roll2 + roll3);
-  }
-//-----------------------------------D12----------------------------------------------------------------------------------------
-  function d121() {
-    moveLog();
-    let roll = Math.floor(Math.random()*12 + 1);
-    document.getElementById("log1").innerHTML = ' (D12: ' + roll + ') = ' + roll;
-  }
-  function d122() {
-    moveLog();
-    let roll1 = Math.floor(Math.random()*12 + 1);
-    let roll2 = Math.floor(Math.random()*12 + 1);
-    document.getElementById("log1").innerHTML = '(D12: ' + roll1 + ') (D12: ' + roll2 + ') = ' +  (roll1 + roll2);
-  }
-
-  function d123() {
-    moveLog();
-    let roll1 = Math.floor(Math.random()*12 + 1);
-    let roll2 = Math.floor(Math.random()*12 + 1);
-    let roll3 = Math.floor(Math.random()*12 + 1);
-    document.getElementById("log1").innerHTML = '(D12: ' + roll1 + ') (D12: ' + roll2 + ') (D12: ' + roll3 + ') = ' +  (roll1 + roll2 + roll3);
-  }
-//-----------------------------------D10----------------------------------------------------------------------------------------
-  function d101() {
-    moveLog();
-    let roll = Math.floor(Math.random()*10 + 1);
-    document.getElementById("log1").innerHTML = ' (D10: ' + roll + ') = ' + roll;
-  }
-  function d102() {
-    moveLog();
-    let roll1 = Math.floor(Math.random()*10 + 1);
-    let roll2 = Math.floor(Math.random()*10 + 1);
-    document.getElementById("log1").innerHTML = '(D10: ' + roll1 + ') (D10: ' + roll2 + ') = ' +  (roll1 + roll2);
-  }
-
-  function d103() {
-    moveLog();
-    let roll1 = Math.floor(Math.random()*10 + 1);
-    let roll2 = Math.floor(Math.random()*10 + 1);
-    let roll3 = Math.floor(Math.random()*10 + 1);
-    document.getElementById("log1").innerHTML = '(D10: ' + roll1 + ') (D10: ' + roll2 + ') (D10: ' + roll3 + ') = ' +  (roll1 + roll2 + roll3);
-  }
-//-----------------------------------D8-----------------------------------------------------------------------------------------
-  function d81() {
-    moveLog();
-    let roll = Math.floor(Math.random()*8 + 1);
-    document.getElementById("log1").innerHTML = ' (D8: ' + roll + ') = ' + roll;
-  }
-  function d82() {
-    moveLog();
-    let roll1 = Math.floor(Math.random()*8 + 1);
-    let roll2 = Math.floor(Math.random()*8 + 1);
-    document.getElementById("log1").innerHTML = '(D8: ' + roll1 + ') (D8: ' + roll2 + ') = ' +  (roll1 + roll2);
-  }
-
-  function d83() {
-    moveLog();
-    let roll1 = Math.floor(Math.random()*8 + 1);
-    let roll2 = Math.floor(Math.random()*8 + 1);
-    let roll3 = Math.floor(Math.random()*8 + 1);
-    document.getElementById("log1").innerHTML = '(D8: ' + roll1 + ') (D8: ' + roll2 + ') (D8: ' + roll3 + ') = ' +  (roll1 + roll2 + roll3);
-  }
-//-----------------------------------D6-----------------------------------------------------------------------------------------
-  function d61() {
-    moveLog();
-    let roll = Math.floor(Math.random()*6 + 1);
-    document.getElementById("log1").innerHTML = ' (D6: ' + roll + ') = ' + roll;
-  }
-  function d62() {
-    moveLog();
-    let roll1 = Math.floor(Math.random()*6 + 1);
-    let roll2 = Math.floor(Math.random()*6 + 1);
-    document.getElementById("log1").innerHTML = '(D6: ' + roll1 + ') (D6: ' + roll2 + ') = ' +  (roll1 + roll2);
-  }
-
-  function d63() {
-    moveLog();
-    let roll1 = Math.floor(Math.random()*6 + 1);
-    let roll2 = Math.floor(Math.random()*6 + 1);
-    let roll3 = Math.floor(Math.random()*6 + 1);
-    document.getElementById("log1").innerHTML = '(D6: ' + roll1 + ') (D6: ' + roll2 + ') (D6: ' + roll3 + ') = ' +  (roll1 + roll2 + roll3);
-  }
-//-----------------------------------D4-----------------------------------------------------------------------------------------
-  function d41() {
-    moveLog();
-    let roll = Math.floor(Math.random()*4 + 1);
-    document.getElementById("log1").innerHTML = ' (D4: ' + roll + ') = ' + roll;
-  }
-  function d42() {
-    moveLog();
-    let roll1 = Math.floor(Math.random()*4 + 1);
-    let roll2 = Math.floor(Math.random()*4 + 1);
-    document.getElementById("log1").innerHTML = '(D4: ' + roll1 + ') (D4: ' + roll2 + ') = ' +  (roll1 + roll2);
-  }
-
-  function d43() {
-    moveLog();
-    let roll1 = Math.floor(Math.random()*4 + 1);
-    let roll2 = Math.floor(Math.random()*4 + 1);
-    let roll3 = Math.floor(Math.random()*4 + 1);
-    document.getElementById("log1").innerHTML = '(D4: ' + roll1 + ') (D4: ' + roll2 + ') (D4: ' + roll3 + ') = ' +  (roll1 + roll2 + roll3);
-  }
-
+}
 
 //------------------------------------------------------------------------------------------------------------------------------
   //move the last rolled number down on the log list.
